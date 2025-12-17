685e073dcc40b55efd098b4f16f6a45bbe7fb8eb
sawone2893 <52617046+sawone2893@users.noreply.github.com>
2025-12-17 20:10:32 +0530

    Create DriverManager.java

---
+++ src/main/java/utilities/DriverManager.java
@@ -0,0 +1,24 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+
+public final class DriverManager {
+
+	private static final ThreadLocal<WebDriver> DRIVER = new ThreadLocal<>();
+
+	private DriverManager() {
+	}
+
+	public static void setDriver(WebDriver driver) {
+		DRIVER.set(driver);
+	}
+
+	public static WebDriver getDriver() {
+		return DRIVER.get();
+	}
+
+	public static void unload() {
+		DRIVER.remove(); // prevent memory leaks after quit
+	}
+
+}
